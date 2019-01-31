51141d461a3fdd6586caff8108291fbb2053a3a7
Do Nhu Vy <vy@bkit.vn>
2019-01-31 22:38:24 +0700

    Update AddItem.js

--- Chapter08/ShoppingList/AddItem.js
+++ Chapter08/ShoppingList/AddItem.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SkyLight from 'react-skylight';
-import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class AddItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-  
-  addItem = () => {
-    const item = {product: this.state.product, amount: this.state.amount};
-    this.props.additem(item);
-    this.addform.hide();
-  }
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
-  }
- 
-  render() {
-    return (
-      <div>
-        <section>
-          <RaisedButton onClick={() => this.addform.show()} label="Add Item" primary={true} />
-        </section>
-        <SkyLight 
-          hideOnOverlayClicked 
-          ref={ref => this.addform = ref} 
-          title="Add item">
-          <TextField type="text" name="product" onChange={this.handleChange} 
-          placeholder="product" /><br/>
-          <TextField type="text" name="amount" onChange={this.handleChange} 
-          placeholder="amount" /><br/>
-          <RaisedButton onClick={this.addItem} label="Add" default={true} />
-        </SkyLight>      
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    addItem = () => {
+        const item =
+            {
+                product: this.state.product,
+                amount: this.state.amount
+            };
+    };
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    };
+
+    render() {
+        return (
+            <div>
+                <section>
+                    <Button onClick={() => this.addform.show()} primary={true}>Add Item</Button>
+                </section>
+
+                <SkyLight hideOnOverlayClicked ref={ref => this.addform = ref} title="Add item">
+                    <TextField type="text" name="product" onChange={this.handleChange} placeholder="product"/><br/>
+                    <TextField type="text" name="amount" onChange={this.handleChange} placeholder="amount"/><br/>
+
+                    <Button onClick={this.addItem} default={true}>Add</Button>
+                </SkyLight>
+            </div>
+        )
+    }
+
 }
 
 export default AddItem;
