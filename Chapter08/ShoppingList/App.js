20a98d18c20b2d70467b6add2c8ace7732674e0a
Do Nhu Vy <vy@bkit.vn>
2019-01-31 22:38:02 +0700

    Update App.js

--- Chapter08/ShoppingList/App.js
+++ Chapter08/ShoppingList/App.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import {List, ListItem} from 'material-ui/List';
-
+import React, {Component} from 'react';
 import './App.css';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import AddItem from './AddItem';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state={ items: [] };
-  }
 
-  addItem = (item) => {
-    this.setState({items: [item, ...this.state.items]});
-  }
-  
-  render() {
-    const listItems = this.state.items.map((item, index) => 
-      <ListItem key={index} secondaryText={item.amount}>{item.product}</ListItem>)
-    return (
-      <div className="App">
-        <h2>Shopping list</h2>
-        <AddItem additem={this.addItem}/>
-        <List>{listItems}</List>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {items: []};
+    }
+
+    addItem = (item) => {
+        this.setState({items: [item, ...this.state.items]});
+    };
+    
+    render() {
+        const listItems = this.state.items.map((item, index) => <ListItem key={index} secondaryText={item.amount}>{item.product}</ListItem>)
+        return (
+            <div className="App">
+                <h2>Shopping list</h2>
+                <AddItem additem={this.addItem}/>
+                <List>{listItems}</List>
+            </div>
+        );
+    }
 }
 
 export default App;
